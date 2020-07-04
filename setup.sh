9613242808591f3b8330015b492fe5aa9c0b4c18
Justin Mangini <justin.mangini@esg.global>
2020-07-04 18:21:56 +0000

    update setup script to install opencv and freenect

--- setup.sh
+++ setup.sh
@@ -1,2 +1,14 @@
 #install pip items
 sudo pip3 install Adafruit-BBIO Adafruit-Blinka odoorpc numpy
+
+#download and run install helpers
+git clone http://github.com/idreamoferp/install_helpers
+cd install_helpers
+
+#install OpenCV for Python 3.7
+git chekcout opencv_python_3
+bash install_opencv_python_3-7-3
+
+#install FreeNECT for Python 3
+git checkout kinect_python3
+bash install_kinect.sh
