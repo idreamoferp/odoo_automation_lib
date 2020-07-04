47df9dc4cc7c3b80f5e7fd239874070b9252cc77
Demo User <debian@beaglebone.localdomain>
2020-07-04 19:11:53 +0000

    fiexed typo

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
+#update apt repos
+sudo apt update && sudo apt upgrade
+
 #install pip items
 sudo pip3 install Adafruit-BBIO Adafruit-Blinka odoorpc numpy
 
@@ -6,7 +9,7 @@ git clone http://github.com/idreamoferp/install_helpers
 cd install_helpers
 
 #install OpenCV for Python 3.7
-git chekcout opencv_python_3
+git checkout opencv_python_3
 bash install_opencv_python_3-7-3
 
 #install FreeNECT for Python 3
