b8735abb285e3d37d6043fe6af8ad2cb6ced579a
Justin Mangini <justin@diyautospec.com>
2020-08-19 15:28:54 -0600

    update test.py with new gpio and conveyor settings

--- tests.py
+++ tests.py
@@ -1,41 +1,43 @@
 import automation, conveyor, automation_web
 import logging, odoorpc, threading, time, argparse
-import digitalio, board #blinka libs
+import digitalio, board, busio #blinka libs
+from adafruit_mcp230xx.mcp23017 import MCP23017
+i2c = busio.I2C(board.SCL, board.SDA)
+mcp20 = MCP23017(i2c, address=0x20)
+#mcp21 = MCP23017(i2c, address=0x21)
 
 #setup logger
 logging.basicConfig(format="%(asctime)s %(levelname)s %(name)s - %(message)s",datefmt='%m/%d/%Y %I:%M:%S %p',level=logging.INFO)
 _logger = logging.getLogger("Test Machine")
 
-class TestMachine(automation.MRP_Automation, automation_web.Automation_Webservice):
+class TestMachine(automation_web.Automation_Webservice, automation.MRP_Automation):
     
     def __init__(self, api, asset_id):
         #init conveyor for this machine
         self.conveyor_1 = Conveyor_1()
+        self.conveyor_1.set_ipm = 10
         
         #setup button pins
-        self.button_start_input = digitalio.DigitalInOut(board.D5)
+        self.button_start_input = digitalio.DigitalInOut(board.D20)
         self.button_start_input.direction = digitalio.Direction.INPUT
         self.button_start_input.pull = digitalio.Pull.UP
         
-        self.button_stop_input = digitalio.DigitalInOut(board.D6)
+        self.button_stop_input = digitalio.DigitalInOut(board.D19)
         self.button_stop_input.direction = digitalio.Direction.INPUT
         self.button_stop_input.pull = digitalio.Pull.UP
         
-        self.button_estop_input = digitalio.DigitalInOut(board.D4)
+        self.button_estop_input = digitalio.DigitalInOut(board.D21)
         self.button_estop_input.direction = digitalio.Direction.INPUT
         self.button_estop_input.pull = digitalio.Pull.UP
         
         self.button_start_led = digitalio.DigitalInOut(board.D17)
         self.button_start_led.direction = digitalio.Direction.OUTPUT
-        self.button_start_led.value = True
         
         self.button_warn_led = digitalio.DigitalInOut(board.D27)
         self.button_warn_led.direction = digitalio.Direction.OUTPUT
-        self.button_warn_led.value = True
         
         self.button_estop_led = digitalio.DigitalInOut(board.D22)
         self.button_estop_led.direction = digitalio.Direction.OUTPUT
-        self.button_estop_led.value = True
         
         super(TestMachine, self).__init__(api, asset_id)
         
@@ -46,7 +48,9 @@ class TestMachine(automation.MRP_Automation, automation_web.Automation_Webservic
         
         self.route_lanes = [MRP_Carrier_Lane_0(self.api, self), MRP_Carrier_Lane_1(self.api, self)]
         
+        
         _logger.info("Machine INIT Compleete.")
+        self.start_webservice()
         return
         
     def button_input_loop(self):
@@ -66,11 +70,11 @@ class TestMachine(automation.MRP_Automation, automation_web.Automation_Webservic
             time.sleep(0.1)
             
     def indicator_start(self, value):
-        self.button_start_led.value = not value
+        self.button_start_led.value = value
         return super(TestMachine, self).indicator_start(value)
     
     def indicator_warn(self, value):
-        self.button_warn_led.value = not value
+        self.button_warn_led.value = value
         return super(TestMachine, self).indicator_warn(value)
         
     def indicator_e_stop(self, value):
@@ -80,41 +84,54 @@ class TestMachine(automation.MRP_Automation, automation_web.Automation_Webservic
     
     #Button inputs
     def button_start(self):
+        self.conveyor_1.start()
         return super(TestMachine, self).button_start()
     
     def button_stop(self):
+        #self.conveyor_1.stop()
         return super(TestMachine, self).button_stop()
     
     def e_stop(self):
         #put render safe i/o here.
+        self.conveyor_1.e_stop()
         return super(TestMachine, self).e_stop()
     
     def e_stop_reset(self):
         #put reboot i/o here
+        self.conveyor_1.e_stop_reset()
         return super(TestMachine, self).e_stop_reset()
+        
+    def quit(self):
+        #set indicators to safe off state.
+        self.indicator_start(False)
+        self.indicator_warn(False)
+        self.conveyor_1.quit()
+        return super(TestMachine, self).quit()  
         
 class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
     def __init__(self, api, mrp_automation_machine):
-        
-        super(MRP_Carrier_Lane_0, self).__init__(api, mrp_automation_machine)
         self._logger = logging.getLogger("Carrier Lane 0")
+        super(MRP_Carrier_Lane_0, self).__init__(api, mrp_automation_machine)
         
-        self._logger.info("Setup GPIO Pins")
-        self.input_ingress = digitalio.DigitalInOut(board.D8)
+        
+        
+        self.input_ingress = mcp20.get_pin(8)
         self.input_ingress.direction = digitalio.Direction.INPUT
         self.input_ingress.pull = digitalio.Pull.UP
         
-        self.ingress_end_stop = digitalio.DigitalInOut(board.D11)
+        self.ingress_end_stop = mcp20.get_pin(9)
         self.ingress_end_stop.direction = digitalio.Direction.INPUT
         self.ingress_end_stop.pull = digitalio.Pull.UP
         
-        self.output_ingress_gate = digitalio.DigitalInOut(board.D23)
+        self.output_ingress_gate = mcp20.get_pin(15)
         self.output_ingress_gate.direction = digitalio.Direction.OUTPUT
-        self.output_ingress_gate.value = True
+        self.output_ingress_gate.value = False
         
-        self.output_carrier_capture = digitalio.DigitalInOut(board.D10)
+        self.output_carrier_capture = mcp20.get_pin(14)
         self.output_carrier_capture.direction = digitalio.Direction.OUTPUT
-        self.output_carrier_capture.value = True
+        self.output_carrier_capture.value = False
+        
+        self._logger.info("Lane INIT Complete")
         pass
     
     #main loop functions
@@ -132,7 +149,7 @@ class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
     def process_ingress(self):
         
         #open ingress gate
-        self.output_ingress_gate.value = False
+        self.output_ingress_gate.value = True
         self._logger.info("Machine opened ingress gate, waiting for product to trigger end stop")
         
         #wait for ingress end stop trigger
@@ -145,8 +162,8 @@ class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
             time.sleep(0.5)
         
         self._logger.info("Product triggered endstop, closing ingress gate, capture product carrier")    
-        self.output_ingress_gate.value = True
-        self.output_carrier_capture.value = False
+        self.output_ingress_gate.value = False
+        self.output_carrier_capture.value = True
         
         self._logger.info("Rotate the product until the carrier home is triggered")
         time.sleep(3)
@@ -163,7 +180,7 @@ class MRP_Carrier_Lane_0(automation.MRP_Carrier_Lane):
         self.mrp_automation_machine.conveyor_1.diverter.divert(self.route_node_lane, destination_lane)
         
         #release carrier capture
-        self.output_carrier_capture.value = True
+        self.output_carrier_capture.value = False
         
         #wait for egress end stop trigger
         time_out = time.time()
@@ -182,15 +199,69 @@ class MRP_Carrier_Lane_1(automation.MRP_Carrier_Lane):
     def __init__(self, api, mrp_automation_machine):
         super(MRP_Carrier_Lane_1, self).__init__(api, mrp_automation_machine)
         self._logger = logging.getLogger("Carrier Lane 1")
+        self._logger.info("Lane INIT Complete")
         pass
 
+import RPi.GPIO as GPIO
 class Conveyor_1(conveyor.Conveyor):
+    
     def __init__(self):
-        super(Conveyor_1, self).__init__()
-        self.diverter = conveyor.Diverter()
+        super(Conveyor_1, self).__init__(name="conveyor_1")
+        
+        self.pid_controller.Kp = .45
+        self.pid_controller.Ki = .05
+        self.pid_controller.Kd = 6
+        
+        self.inch_per_rpm = 4.75
+        
+        GPIO.setmode(GPIO.BCM)  
+        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.add_event_detect(23, GPIO.RISING, callback=self.tach_tick)
+        
+        GPIO.setup(12,GPIO.OUT)
+        self.motor_p = GPIO.PWM(12,5000)
+        self.motor_p.start(0)
+        self.motor_duty = 40
+        
+        #setup diverter logic
+        self.diverter = conveyor.Diverter("Conveyor_1_Diverter")
         self.diverter.lane_diverter = {'work':{'work':self.diverter_work_work,'bypass':self.diverter_work_bypass},'bypass':{'work':self.diverter_bypass_work,'bypass':self.diverter_bypass_bypass}}
         pass
     
+    def set_speed(self, freq_offset):
+        duty = self.motor_duty + freq_offset
+        
+        if duty > 100:
+            duty = 100
+            
+        if duty < 0:
+            duty = 0
+        
+        self.motor_p.ChangeDutyCycle(duty)
+        self.motor_duty = duty
+        
+        return super(Conveyor_1, self).set_speed(freq_offset=freq_offset)
+    
+    def start(self):
+        self.motor_p.ChangeDutyCycle(self.motor_duty)
+        return super(Conveyor_1, self).start()
+    
+    def stop(self):
+        self.motor_p.ChangeDutyCycle(0)
+        return super(Conveyor_1, self).stop()
+    
+    def tach_tick(self, ch):
+        new_tick = time.time()
+        if self.last_tach_tick == 0:
+            self.last_tach_tick = new_tick
+            return
+        
+        pulse_len = new_tick - self.last_tach_tick
+        if pulse_len > 0.8:
+            self.last_tach_tick = new_tick
+            self.current_ipm = round(pulse_len * self.inch_per_rpm, 1)
+            return super(Conveyor_1, self).tach_tick()
+
     def diverter_work_work(self):
         _logger.info("Setting Diverter from Work to Work")
         pass
@@ -206,14 +277,19 @@ class Conveyor_1(conveyor.Conveyor):
     def diverter_bypass_bypass(self):
         _logger.info("Setting Diverter from Bypass to Bypass")
         pass
+    
+    def quit(self):
+        self.stop()
+        self.motor_p.stop()
+        return super(Conveyor_1, self).quit()  
 
 #startup this machine
 if __name__ == "__main__":
     
     #odoo api settings, TODO: move these to a server_config file
-    server = "esg-beta.idreamoferp.com"
-    port = 80
-    database = "ESG_Beta_1-0"
+    server = "usboiepxd01"
+    port = 8069
+    database = "ESG_Beta_0-1"
     user_id = "equipment_072"
     password = "1q2w3e4r"
     
@@ -231,9 +307,7 @@ if __name__ == "__main__":
     
     #create instance of this test machine, and start its engine
     test_machine = TestMachine(api=odoo, asset_id=args.equipment_id)
-    
-    #launch web service
-    test_machine.start_webservice()
+
     
     #test_machine.button_start()
     
