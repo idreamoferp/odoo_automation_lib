1bbed4539eb9258c6d1bd9dbcf3302e03916ade2
Justin Mangini <justin@diyautospec.com>
2020-08-25 13:55:51 -0600

    update wortk offsets

--- motion_control.py
+++ motion_control.py
@@ -1,37 +1,49 @@
 
 
-class MotonControl():
-    def __init__(self, config):
-        self.config = config
+class MotonControl(object):
+    def __init__(self):
+        self.status = False
+        self.coordinate_system = False
+        
         self.position_x = 0.0
         self.position_y = 0.0
         self.position_z = 0.0
+        self.position_a = 0.0
+        self.position_b = 0.0
+        
         self.work_offset_x = 0.0
         self.work_offset_y = 0.0
         self.work_offset_z = 0.0
-        self.actual_feedrate = 0.0
-        self.actual_spindle = 0.0
-        self.override_feed = 100
-        self.override_rapids = 100
-        self.override_spindle = 100
-        self.errors = []
-        
-    pass
-
-    def goto_position_abs(self,x=False,y=False,z=False,a=False,b=False,feed=False):
-        self._goto_position(x,y,z,a,b,feed)
-        return True
-   
-    def goto_position_rel(self,x=False,y=False,z=False,a=False,b=False,feed=False):
-        self._goto_position(x,y,z,a,b,feed)
-        return True
-       
-    def _goto_position(self,x=False,y=False,z=False,a=False,b=False,feed=False):
-        return True
-   
-    def home(self):
-        return True
-        
+        self.work_offset_a = 0.0
+        self.work_offset_b = 0.0
+        pass
+    
     def work_offset(self,x=0,y=0,z=0,a=0,b=0):
-        return True
+        self.work_offset_x = x
+        self.work_offset_y = y
+        self.work_offset_z = z
+        self.work_offset_a = a
+        self.work_offset_b = b
+        pass
+    
+    def home(self):
+        self.work_offset_x = self.position_x
+        self.work_offset_y = self.position_y
+        self.work_offset_z = self.position_z
+        self.work_offset_a = self.position_a
+        self.work_offset_b = self.position_b
         
+        return True
+    
+    def _goto_position(self,x=False,y=False,z=False,a=False,b=False,feed=False):
+        pass
+    
+    def goto_position_rel(self,x=False,y=False,z=False,a=False,b=False,feed=False):
+        self._goto_position(x+self.position_x, y+self.position_y, z+self.position_z, a+self.position_a, b+self.position_b, feed)
+        pass
+    
+    def goto_position_abs(self,x=False,y=False,z=False,a=False,b=False,feed=False):
+        self._goto_position(x+self.work_offset_x, y+self.work_offset_y, z+self.work_offset_z, a+self.work_offset_a, b+self.work_offset_b, feed)
+        pass
+    
+    
