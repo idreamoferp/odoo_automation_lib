81359a4a4564f4acb6eb8e6265f322d382e7370c
Justin Mangini <justin.mangini#esg.global>
2021-12-18 19:35:36 +0000

    update package and requires for cable harness module

--- setup.py
+++ setup.py
@@ -4,7 +4,5 @@ setup(
     name='odoo_automation', 
     version='13.0', 
     packages=find_packages(),
-    install_requires=[
-        'Adafruit-Blinka', 'simple-pid', 'RPi.GPIO', 'Flask==1.1.2', 'numpy', 'OdooRPC'
-        ]
+    install_requires=['Adafruit-Blinka', 'simple-pid', 'RPi.GPIO', 'Flask==1.1.2', 'numpy', 'OdooRPC', 'smbus2']
     )
